07fd186903e94bbb797840599fe55a223baeaa1a
xuetaibo <xuebojob@163.com>
2022-06-15 01:05:52 +0800

    master

--- hal_key.c
+++ hal_key.c
@@ -8,3 +8,5 @@ uint8_t keyStep[KEY_NUM];                //按键检测流程
 uint16_t keyDebounceTime[KEY_NUM];       //按键消抖时间
 uint16_t keyLongPressTime[KEY_NUM];      //按键长按时间
 uint16_t keyContinuePressTime[KEY_NUM];  //按键连续按下时间
+
+void keyProc() {}
