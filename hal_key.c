5bba761e05dc0aa8327c7950f8517ef9fa576b77
xuetaibo <xuebojob@163.com>
2022-06-18 16:47:02 +0800

    第二次修改

--- hal_key.c
+++ hal_key.c
@@ -2,11 +2,132 @@
 
 #include <stdio.h>
 
-KEY_CALLBACK key_callback = NULL;
+KeyEvent_CallBack_t KeyScanCBS;
 
-uint8_t keyStep[KEY_NUM];                //按键检测流程
-uint16_t keyDebounceTime[KEY_NUM];       //按键消抖时间
-uint16_t keyLongPressTime[KEY_NUM];      //按键长按时间
-uint16_t keyContinuePressTime[KEY_NUM];  //按键连续按下时间
+static uint8_t hal_keyConfig(void);
 
-void keyProc() {}
+static uint8_t hal_getKeyS2Sta(void);
+static uint8_t hal_getKeyS3Sta(void);
+static uint8_t hal_getKeyS4Sta(void);
+
+uint8_t (*getKeyState[KEY_NUM])(void) = {
+    hal_getKeyS2Sta,
+    hal_getKeyS3Sta,
+    hal_getKeyS4Sta,
+};
+
+uint8_t keyStep[KEY_NUM];            //按键检测流程
+uint16_t keyScanTime[KEY_NUM];       //按键消抖时间
+uint16_t keyPressLongTime[KEY_NUM];  //按键长按时间
+uint16_t keyContPressTime[KEY_NUM];  //按键连续按下时间
+
+void hal_keyInit(void) {
+  uint8_t i;
+  KeyScanCBS = 0;
+  hal_keyConfig();
+
+  for (i = 0; i < KEY_NUM; i++) {
+    keyStep[i] = KEY_STEP_WAIT;
+    keyScanTime[i] = KEY_SCANTIME;
+    keyPressLongTime[i] = KEY_PRESS_LONG_TIME;
+    keyContPressTime[i] = KEY_PRESS_CONTINUE_TIME;
+  }
+}
+
+void hal_KeyScanCBSRegister(KeyEvent_CallBack_t pCBS) {
+  if (KeyScanCBS == 0) {
+    KeyScanCBS = pCBS;
+  }
+}
+
+void hal_keyProc(void) {
+  uint8_t i, KeyState[KEY_NUM];
+  uint16_t keys;
+  uint16_t state;
+
+  for (i = 0; i < KEY_NUM; i++) {
+    keys = 0;
+    state = 0;
+    KeyState[i] = getKeyState[i]();
+    switch (keyStep[i]) {
+      case KEY_STEP_WAIT:  //等待按键按下
+        if (KeyState[i]) {
+          keyStep[i] = KEY_STEP_CLICK;
+        }
+        break;
+      case KEY_STEP_CLICK:  //按键按下
+        if (KeyState[i]) {
+          if (!(--keyScanTime[i])) {
+            keyScanTime[i] = KEY_SCANTIME;
+            keyStep[i] = KEY_STEP_LONG_PRESS;
+            keys = i + 1;
+            state = KEY_CLICK;
+          } else {
+            keyScanTime[i] = KEY_SCANTIME;
+            keyStep[i] = KEY_STEP_WAIT;
+          }
+          break;
+          case KEY_STEP_LONG_PRESS:  //按键长按
+            if (KeyState[i]) {
+              if (!(--keyPressLongTime[i])) {
+                keyPressLongTime[i] = KEY_PRESS_LONG_TIME;
+                keyStep[i] = KEY_STEP_WAIT;
+                keys = i + 1;
+                state = KEY_LONG_PRESS;
+              } else {
+                keyPressLongTime[i] = KEY_PRESS_LONG_TIME;
+                keyStep[i] = KEY_STEP_WAIT;
+                keys = i + 1;
+                state = KEY_CLICK_RELEASE;
+              }
+            }
+            break;
+          case KEY_STEP_CONTINUOUS_PRESS:  //按键连续按下
+            if (KeyState[i]) {
+              if (!(--keyContPressTime[i])) {
+                keyContPressTime[i] = KEY_PRESS_CONTINUE_TIME;
+                keyStep[i] = KEY_STEP_WAIT;
+                keys = i + 1;
+                state = KEY_LONG_PRESS_CONTINUOUS;
+              } else {
+                keyContPressTime[i] = KEY_PRESS_CONTINUE_TIME;
+                keyStep[i] = KEY_STEP_WAIT;
+                keys = i + 1;
+                state = KEY_LONG_PRESS_RELEASE;
+              }
+            }
+            break;
+        }
+    }
+  }
+}
+
+//----------------
+static uint8_t hal_getKeyS2Status(void) {
+  return (!GPIO_ReadInputDataBit(S2_PORT, S2_PIN));
+}
+
+static uint8_t hal_getKeyS3Status(void) {
+  return (!GPIO_ReadInputDataBit(S3_PORT, S3_PIN));
+}
+
+static uint8_t hal_getKeyS4Status(void) {
+  return (!GPIO_ReadInputDataBit(S4_PORT, S4_PIN));
+}
+
+//----------------
+static uint8_t hal_getKeyS2Sta(void) {
+  uint8_t sta = 0;
+  if (hal_getKeyS2Status()) {
+    sta = 1;
+  }
+  return sta;
+}
+//按键扫描
+static uint8_t hal_getKeyS3Sta(void) {
+  uint8_t sta = 0;
+  if (hal_getKeyS3Status()) {
+    sta = 1;
+  }
+  return sta;
+}
--- hal_key.h
+++ hal_key.h
@@ -21,32 +21,40 @@ typedef enum { KEY_S2, KEY_S3, KEY_S4, KEY_NUM } KEY_TYPEDEF;
 
 //按键的状态
 typedef enum {
-  KEY_IDLE,                 //空闲
-  KEY_PRESS,                //按下
-  KEY_PRESS_RELEASE,        //按下并释放
-  KEY_LONG_PRESS,           //长按
-  KEY_LONG_PRESS_CONTINUE,  //长按继续
-  KEY_LONG_PRESS_RELEASE,   //长按释放
-} KEY_STATUS;
+  KEY_IDLE,                   //空闲
+  KEY_CLICK,                  //按下
+  KEY_CLICK_RELEASE,          //按下并释放
+  KEY_LONG_PRESS,             //长按
+  KEY_LONG_PRESS_CONTINUOUS,  //长按继续
+  KEY_LONG_PRESS_RELEASE,     //长按释放
+} KEY_EVENT_TYPEDEF;
 
 //按键检测过程
 typedef enum {
-  KEY_STEP_WAIT,            //等待按键按下
-  KEY_STEP_PRESS,           //按键按下
-  KEY_STEP_LONG_PRESS,      //按键长按
-  KEY_STEP_CONTINUE_PRESS,  //按键持续按下
+  KEY_STEP_WAIT,              //等待按键按下
+  KEY_STEP_CLICK,             //按键按下
+  KEY_STEP_LONG_PRESS,        //按键长按
+  KEY_STEP_CONTINUOUS_PRESS,  //按键持续按下
 } KEY_STEP_TYPEDEF;
 
 //定义一个函数指针类型
-typedef void (*KEY_CALLBACK)(KEY_TYPEDEF key_type, KEY_STATUS key_status);
+typedef void (*KeyEvent_CallBack_t)(KEY_TYPEDEF keys, KEY_EVENT_TYPEDEF state);
 
 //按键扫描定时器Tick,每个Tick时间为1ms
-#define KEY_SACN_TICK_MS 10  // 10ms
+#define KEY_SCANT_TICK 10  // 10ms
 
 //按键消抖时间,以10ms为Tick
-#define KEY_DEBOUNCE_TICK_MS 2  // 20ms
+#define KEY_SCANTIME 2  // 20ms
 
 //连续长按时间,以10ms为Tick
-#define KEY_LONG_PRESS_TICK_MS 200  // 2s
+#define KEY_PRESS_LONG_TIME 200  // 2s
+
+//持续长按间隔时间
+#define KEY_PRESS_CONTINUE_TIME 15  // 150ms
+
+void hal_KeyInit(void);
+void hal_keyProc(void);
+void hal_KeyScanCBSRegister(KeyEvent_CallBack_t pCBS);
+void hal_KeyScan(void);
 
 #endif
